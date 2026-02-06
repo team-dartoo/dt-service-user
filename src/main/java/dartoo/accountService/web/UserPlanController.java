f86e721acd0c7b3a98a18f767df02a98b3601f96
baekJiHoon <bgh1234554@gmail.com>
2026-02-06 17:01:19 +0900

    컨트롤러 엔드포인트 오타 수정

--- src/main/java/dartoo/accountService/web/UserPlanController.java
+++ src/main/java/dartoo/accountService/web/UserPlanController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/user/plan")
+@RequestMapping("/api/users/plan")
 public class UserPlanController {
     private final UserPlanService userPlanService;
 
